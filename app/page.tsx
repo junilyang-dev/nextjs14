6bdc6aa1ab08ff6ee08ebc6ae43e19f872e0eb98
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-08-12 10:41:24 +0000

    Layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Navigation from "../components/navigation";
-
-export default function Page() {
-  return (
-    <div>
-      <Navigation />
-      <h1>Hello!</h1>
-    </div>
-  );
-}
+    export default function Page() {
+      return (
+        <div>
+          <h1>Hello!</h1>
+        </div>
+      );
+    }
