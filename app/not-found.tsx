74e496b4ae26e77510b00cd25e183b97fc1db7b1
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-08-27 16:51:01 +0000

    not found link home

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Not found",
 };
-
 export default function NotFound() {
   return (
     <div>
       <h1>Not found!</h1>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
     </div>
   );
 }
